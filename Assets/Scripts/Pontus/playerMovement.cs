e0cd898d3dc75ac0b98572da592986ef7d127196
UTB\hummpo <pontus.hummerhielm@gmail.com>
2017-02-13 16:02:20 +0100

    Pontus first commit

---
+++ Assets/Scripts/Pontus/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    //I'm going to use NavMeshComponents to create proper pathing!!!!!!!
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
+        
+        //    if(Physics.Raycast(ray, out hit))
+        //    {
+                
+        //    }
+
+        //}
+    }
+}
