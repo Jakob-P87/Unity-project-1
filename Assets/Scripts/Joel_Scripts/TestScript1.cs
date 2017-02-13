c98486edad76605a0754907baeb9943e8ff0eb9a
Joel Åman <Schoelxz@gmail.com>
2017-02-13 13:13:31 +0100

    Påbörjat ny scen+script (Joel).

---
+++ Assets/Scripts/Joel_Scripts/TestScript1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript1 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("WHAT?");
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+
+	}
+}
+
+public class SubClassOne : TestScript1
+{
+
+    public int oneInt;
+   
+
+}
+
+public class SubClassTwo : TestScript1
+{
+
+    public int twoInt;
+
+}
