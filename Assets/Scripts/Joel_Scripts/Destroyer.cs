9dcb453e243168a2cc2271a03a2e3ee95b462444
Joel Åman <Schoelxz@gmail.com>
2017-02-15 01:47:54 +0100

    Testar ut scripts för träning och dev.

---
+++ Assets/Scripts/Joel_Scripts/Destroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        Destroy(other.gameObject);
+
+    }
+}
