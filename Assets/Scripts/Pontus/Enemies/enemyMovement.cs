f1baf23f8f88319900e6847af397a878d554238c
UTB\hummpo <pontus.hummerhielm@gmail.com>
2017-02-15 12:15:20 +0100

    Minor tweaks

--- Assets/Scripts/Pontus/Enemies/enemyMovement.cs
+++ Assets/Scripts/Pontus/Enemies/enemyMovement.cs
@@ -5,19 +5,18 @@ using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour {
 
-    NavMeshAgent agent;
     public GameObject target;
-    enemyStates enemyState;
     private float distToTarget;
     Vector3 lastSeen;
     Vector3 startPos;
+    NavMeshAgent agent;
+    enemyStates enemyState;
 
-	void Start ()
+    void Start ()
     {
         enemyState = enemyStates.IDLE; 
         agent = GetComponent<NavMeshAgent>();
         startPos = transform.position;
-
     }
 	
 	void Update ()
@@ -25,11 +24,9 @@ public class enemyMovement : MonoBehaviour {
         distToTarget = Vector3.Distance(transform.position, target.transform.position);
         RaycastHit hit;
         var castDir = target.transform.position - transform.position;
-
         Physics.SphereCast(transform.position, 1, castDir, out hit);
-        //Debug.Log(hit.collider.tag);
 
-        if (distToTarget < 75 && hit.collider.tag == "Player")
+        if (distToTarget < 60 && hit.collider.tag == "Player")
         {
             lastSeen = target.transform.position;
             enemyState = enemyStates.CHASE;
@@ -54,4 +51,12 @@ public class enemyMovement : MonoBehaviour {
                 break;
         }
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody)
+        {
+            Debug.Log("Hit!");
+        }
+    }
 }
