0d739c7bdeb4ef05ddfc60e0e35059c0f572fd82
Joel Åman <Schoelxz@gmail.com>
2017-02-24 12:34:26 +0100

    Added Fog script.

---
+++ Assets/Scripts/Joel_Scripts/Weather/Fog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour {
+
+    //Default Fog Values
+    public int fogStart = 20;
+    public int fogEnd = 31;
+
+    void Start()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = Color.white;
+        RenderSettings.fogMode = FogMode.Linear;
+    }
+
+    void Update()
+    {
+        RenderSettings.fogStartDistance = fogStart;
+        RenderSettings.fogEndDistance = fogEnd;
+    }
+}
