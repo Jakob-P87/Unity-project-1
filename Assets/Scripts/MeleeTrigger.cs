e68ed3f3b3ffb78a2a5e87f3fff30f986aaf4667
Joel Åman <Schoelxz@gmail.com>
2017-02-24 09:47:47 +0100

    Comentted out 2 scripts

--- Assets/Scripts/MeleeTrigger.cs
+++ Assets/Scripts/MeleeTrigger.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class MeleeTrigger : MonoBehaviour {
+//public class MeleeTrigger : MonoBehaviour {
 
-    GameObject player;
+//    GameObject player;
 
-  private float damage = 50.0f;
+//  private float damage = 50.0f;
  
-void Start()
-    {
-        Physics.IgnoreCollision(collider, player.collider);
-    }
+//void Start()
+//    {
+//        Physics.IgnoreCollision(collider, player.collider);
+//    }
 
-    void OnTriggerEnter(Collider other)
-    {
+//    void OnTriggerEnter(Collider other)
+//    {
 
-        if (other.GetComponent(EnemyDamage))
-        {
-            other.gameObject.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
-            gameObject.active = false;
-        }
+//        if (other.GetComponent(EnemyDamage))
+//        {
+//            other.gameObject.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+//            gameObject.active = false;
+//        }
 
-    }
-}
+//    }
+//}
 
--- Assets/Scripts/PlayerMelee.cs
+++ Assets/Scripts/PlayerMelee.cs
@@ -1,38 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PlayerMelee : MonoBehaviour {
+//public class PlayerMelee : MonoBehaviour
+//{
 
-GameObject player;
-GameObject meleeTrigger;
-float swingRate = 0.5f;
+//    GameObject player;
+//    GameObject meleeTrigger;
+//    float swingRate = 0.5f;
 
-    void Start()
-    {
-        meleeTrigger.active = false;
-        MeleeSwing();
-    }
+//    void Start()
+//    {
+//        meleeTrigger.active = false;
+//        MeleeSwing();
+//    }
 
-    void MeleeSwing()
-    {
-        while (true)
-        {
-            if (Input.GetButtonUp("Fire1"))
-            {
+//    void MeleeSwing()
+//    {
+//        while (true)
+//        {
+//            if (Input.GetButtonUp("Fire1"))
+//            {
 
-                player.animation.CrossFade("1h_attack1");
-                meleeTrigger.active = true;
-                yield WaitForSeconds(player.animation["1h_attack1"].length);
-                meleeTrigger.active = false;
-                break;
+//                player.animation.CrossFade("1h_attack1");
+//                meleeTrigger.active = true;
+//                yield WaitForSeconds(player.animation["1h_attack1"].length);
+//                meleeTrigger.active = false;
+//                break;
 
-                yield WaitForSeconds(swingRate);
+//                yield WaitForSeconds(swingRate);
 
-            }
-            else
-            {
-                yield;
-            }
-        }
-    }
+//            }
+//            else
+//            {
+//                yield;
+//            }
+//        }
+
+//    }
+//}
