298c6cd095f21652a2dc43c22527d2787a98ff7b
Jakob <knasenpersson@gmail.com>
2017-02-28 16:53:05 +0100

    Not that much done in this commit.

--- Assets/Scripts/Jakob/CharacterGUI.cs
+++ Assets/Scripts/Jakob/CharacterGUI.cs
@@ -6,10 +6,21 @@ using UnityEngine.UI;
 public class CharacterGUI : MonoBehaviour
 {
     private UserStats stats;
+
+    [SerializeField]
+    private Text playerStr;
+    [SerializeField]
+    private Text playerVit;
+    [SerializeField]
+    private Text playerDex;
+
     [SerializeField]
     private Text playerName;
     [SerializeField]
     private Text playerHp;
+    [SerializeField]
+    private Text playerXp;
+
     [SerializeField]
     private GameObject panel;
 
@@ -36,7 +47,11 @@ public class CharacterGUI : MonoBehaviour
     void UpdateTextfields()
     {
         playerName.text = stats.username + " Lv: " + stats.level.ToString();
-        playerHp.text = "Hp: " + stats.maxHp.ToString();
+        playerHp.text = "Current Hp:    " + stats.maxHp.ToString();
+        playerXp.text = "Current Exp:   " + stats.curXp.ToString();
+        playerStr.text = "Str:          " + stats.curStrength.ToString();
+        playerVit.text = "Vit:          " + stats.curVitality.ToString();
+        playerDex.text = "Dex:          " + stats.curDexterity.ToString();
     }
 
     public void Toggle()
