d9462c7c7d0ea986b00d5d1910e79b396ed29d46
Tommy Sundberg <tommy.e.sundberg@hotmail.com>
2017-03-01 13:59:53 +0100

    some weapon stuff

---
+++ Assets/Scripts/Tommy/Sword.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour, IWeapon
+{
+    public List<UserStats> Stats { get; set; }
+
+    public void PerformAttack()
+    {
+        Debug.Log("Sword attack!");
+    }
+}
