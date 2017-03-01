877372159c6297e09d968a10eef6236129e37d98
Kevin Holmgren <ketoho@hotmail.com>
2017-03-01 16:10:36 +0100

    Spider will now be destroyed if health goes down to 0 and has a 30% chace to drop a mushroom.

---
+++ Assets/Scripts/ItemDrop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDrop : MonoBehaviour {
+    public Transform drop;
+    public Transform pos;
+
+    float health;
+
+    // Use this for initialization
+    void Start () {
+        //reference to spider object with the script enemyUI on itself
+        GameObject spider_myOldOne = GameObject.Find("spider_myOldOne");
+        enemyUI sRef = spider_myOldOne.GetComponent<enemyUI>();
+        health = sRef.currentHp; //health = enemyUI's currentHp
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0) //if currentHp <= 0
+        {
+            Destroy(gameObject); //removes the object
+            if (Random.value <= 0.3) //%30 percent chance to happen (1 = 100%)
+            {
+                Instantiate(drop, pos.position, pos.rotation); //creates object on a position (choose in the editor!)
+            }
+        }
+	}
+}
