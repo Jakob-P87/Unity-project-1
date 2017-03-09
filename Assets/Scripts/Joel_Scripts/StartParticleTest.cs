0ff6a47da0b38c30ecead71b9289a98f39b59933
Joel Åman <Schoelxz@gmail.com>
2017-03-09 02:58:11 +0100

    Player now has a particle effect playing when leveling up.

--- Assets/Scripts/Joel_Scripts/StartParticleTest.cs
+++ Assets/Scripts/Joel_Scripts/StartParticleTest.cs
@@ -4,57 +4,10 @@ using UnityEngine;
 
 public class StartParticleTest : MonoBehaviour {
 
-    ParticleSystem system
+    public void PlayPartEff()
     {
-        get
-        {
-            if (_CachedSystem == null)
-                _CachedSystem = GetComponent<ParticleSystem>();
-            return _CachedSystem;
-        }
-    }
-    private ParticleSystem _CachedSystem;
-
-    public Rect windowRect = new Rect(0, 0, 300, 120);
-
-    public bool includeChildren = true;
-
-    void OnGUI()
-    {
-        windowRect = GUI.Window("ParticleController".GetHashCode(), windowRect, DrawWindowContents, system.name);
+        var lvl = GetComponent<ParticleSystem>();
+        lvl.Play();
     }
 
-    void DrawWindowContents(int windowId)
-    {
-        if (system)
-        {
-            GUILayout.BeginHorizontal();
-            GUILayout.Toggle(system.isPlaying, "Playing");
-            GUILayout.Toggle(system.isEmitting, "Emitting");
-            GUILayout.Toggle(system.isPaused, "Paused");
-            GUILayout.EndHorizontal();
-
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("Play"))
-                system.Play(includeChildren);
-            if (GUILayout.Button("Pause"))
-                system.Pause(includeChildren);
-            if (GUILayout.Button("Stop Emitting"))
-                system.Stop(includeChildren, ParticleSystemStopBehavior.StopEmitting);
-            if (GUILayout.Button("Stop & Clear"))
-                system.Stop(includeChildren, ParticleSystemStopBehavior.StopEmittingAndClear);
-            GUILayout.EndHorizontal();
-
-            includeChildren = GUILayout.Toggle(includeChildren, "Include Children");
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Time(" + system.time + ")");
-            GUILayout.Label("Particle Count(" + system.particleCount + ")");
-            GUILayout.EndHorizontal();
-        }
-        else
-            GUILayout.Label("No particle system found");
-
-        GUI.DragWindow();
-    }
 }
