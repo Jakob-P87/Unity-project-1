65a6b5faf5535cede1d4c1bc15bc7df62805150a
DESKTOP-OF-KEBA\Kevin <ketoho@hotmail.com>
2017-03-22 19:47:28 +0100

    Added hotkey ("M") for toggling mini map on the release, scene

---
+++ Assets/Scripts/ShowMiniMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMiniMap : MonoBehaviour {
+
+    public Inventory inventory;
+
+    bool showMiniMap = false;
+
+    [SerializeField]
+    GameObject Minimap;
+
+    void Start ()
+    {
+        //Minimap.SetActive(showMiniMap);
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        showMiniMap = !showMiniMap;
+        Minimap.SetActive(showMiniMap);
+    }
+}
